8d93aa6f59ebe1be68d70c4c0082379a3ae0f650
KevinWorkMy <31151502+KevinWorkMy@users.noreply.github.com>
2017-08-23 13:23:21 +0800

    Create SportType.java
    体育类型

---
+++ SportType.java
@@ -0,0 +1,24 @@
+public enum SportType {
+    Soccer = 10, //足球
+    Basketball = 11, //篮球
+    Tennis = 12, //网球
+    CounterStrike = 13, //反恐精英
+    Dota2 = 14, //刀塔2
+    LOL = 15, //英雄联盟
+    StarCraft2 = 16, //星际争霸2
+    Baseball = 17, //棒球
+    AmericanFootball = 18, //美式足球
+    IceHockey = 19, //冰球
+    Snooker = 20, //斯诺克
+    Golf = 21, //高尔夫球
+    Volleyball = 22, //排球
+    Handball = 23, //手球
+    RugbyLeague = 24, //联盟式橄榄球
+    BeachVolleyball = 25, //沙滩排球
+    Badminton = 26, //羽毛球
+    Bowling = 27, //保龄球
+    Darts = 28, //飞镖
+    Squash = 29, //壁球
+    Cricket = 30, //板球
+}
+                        
