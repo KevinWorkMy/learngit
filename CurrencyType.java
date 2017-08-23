72f412f99580486520632bc975c5434574c6d97d
KevinWorkMy <31151502+KevinWorkMy@users.noreply.github.com>
2017-08-23 13:36:58 +0800

    Create CurrencyType.java
    货币类型

---
+++ CurrencyType.java
@@ -0,0 +1,12 @@
+public enum CurrencyType {
+    CNY = 1, //人民币
+    USD = 2, //美元
+    EUR = 3, //欧元
+    GBP = 4, //英镑
+    JPY = 5, //日元
+    KRW = 6, //韩元
+    HKD = 7, //港币
+    AUD = 8, //澳元
+    CAD = 9, //加元
+    PHP = 10, //菲律宾比索
+}
